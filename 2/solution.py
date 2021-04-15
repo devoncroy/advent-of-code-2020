a35c729325f58c3c5478253c10795fb5188a46ea
Devon Croy <devoncroy@Devons-MacBook-Air.local>
2021-04-15 00:24:32 -0600

    day 2 solution 2

--- 2/solution.py
+++ 2/solution.py
@@ -28,7 +28,25 @@ def solution(data):
 
     return count
 
+def solution2(data):
+    count = 0
+
+    for item in data:
+        temp = item['rule'].split(' ')
+        character = temp[-1]
+        temp = temp[0].split('-')
+        first = int(temp[0].strip()) - 1
+        second =int(temp[1].strip()) - 1
+        first = item['password'][first] == character
+        second = item['password'][second] == character
+        if (first or second) and not (first and second):
+            count += 1
+
+    return count
+
 if __name__ == "__main__":
     data = loadData()
     answer = solution(data)
+    print(answer)
+    answer = solution2(data)
     print(answer)
